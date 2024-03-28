ca1908a369c3419bd1ab1789c8e1fd88b007b526
José Victor <victorlimaf55@gmail.com>
2024-03-28 02:47:31 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,5 +2,5 @@ from GA import GeneticAlgorithm
 
 if __name__ == '__main__':
     GA = GeneticAlgorithm()
-    best = GA.run()
-    print(f'Beste Individual:\n {best}')
+    bestIndividual = GA.run()
+    print(f'Beste Individual:\n {bestIndividual}')
